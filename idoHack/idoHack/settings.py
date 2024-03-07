883b5f882313cc61e70ece77268c273b3fb889f5
kalKun24 <grenouille24hi@gmail.com>
2024-03-07 17:42:17 +0900

    :recycle: #DBの接続情報の変更

--- idoHack/idoHack/settings.py
+++ idoHack/idoHack/settings.py
@@ -63,12 +63,8 @@ WSGI_APPLICATION = 'idoHack.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
