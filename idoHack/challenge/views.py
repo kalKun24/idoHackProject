8647ed1ca9f95a29953af4a2cdbbcd387ba1d17e
kalKun24 <grenouille24hi@gmail.com>
2024-03-12 20:27:43 +0900

    :sparkles: #base.htmlを作成, exerciseModel, ChallengeModelのTemplateを作成

--- idoHack/challenge/views.py
+++ idoHack/challenge/views.py
@@ -9,19 +9,26 @@ class ExerciseListView(ListView):
     context_object_name = 'exercise_list'
 
 
-#ToDo: Exercise内のChallengeを表示するViewを作成
 class ExerciseDetailView(DetailView):
     model = ExerciseModel
     template_name = 'challenge/exercise_detail.html'
     context_object_name = 'exercise_detail'
 
+class ChallengeListView(ListView):
+    model = ChallengeModel
+    template_name = 'challenge/challenge_list.html'
+    context_object_name = 'challenge_list'
+
+    # id=pkのExercise内のChallengeを取得
+    def get_queryset(self) -> Any:
+        return ChallengeModel.objects.filter(exercise_title=self.kwargs['pk'])
+
+    # id=pkのExerciseを取得
     def get_context_data(self, **kwargs: Any) -> Any:
         context = super().get_context_data(**kwargs)
-        context['challenge_list'] = ChallengeModel.objects.filter(exercise_title=self.object)
-        context['exercise_id'] = self.kwargs['pk']
+        context['exercise_detail'] = ExerciseModel.objects.get(pk=self.kwargs['pk'])
         return context
     
-#ToDo: Challengeの内容を表示するViewを作成
 class ChallengeDetailView(DetailView):
     model = ChallengeModel
     template_name = 'challenge/challenge_detail.html'
@@ -31,9 +38,8 @@ class ChallengeDetailView(DetailView):
     def get_object(self, queryset=None) -> Any:
         return ChallengeModel.objects.get(pk=self.kwargs['pks'])
 
-    # id=pkのExercise内のChallengeを取得
+    # id=pkのExerciseを取得
     def get_context_data(self, **kwargs: Any) -> Any:
         context = super().get_context_data(**kwargs)
-        context['challenge_list'] = ChallengeModel.objects.filter(exercise_title=self.kwargs['pk'])
-        context['exercise_id'] = self.kwargs['pk']
+        context['exercise_detail'] = ExerciseModel.objects.get(pk=self.kwargs['pk'])
         return context
